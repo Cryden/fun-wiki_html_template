729fe1b960395f8ff1c42e3f47889de33c410666
CRYDEsigN <Cryden_da@mail.ru>
2017-11-01 14:02:06 +0300

    js modules fixed

--- source/js/app.js
+++ source/js/app.js
@@ -1,45 +1,11 @@
-require('./bootstrap');
+// load JQuery
+
+window.$ = window.jQuery = require('jquery');
+
+// Load fonts
 
 require ('./modules/font_loader.js');
+
 //Display menu function
 
-function menu_display() {
-    var display_menu = true;
-    console.log('menu display init');
-    $('.header-menu-open-button, .header-menu-close-button').click(function() {
-        //console.log('click');
-        if (display_menu === false) {
-            $("#main_menu").animate({
-                left: "-=320",
-            }, 500);
-            $(".overlay").hide(0);
-            $(".header-menu-open-button").show(500);
-            $(".header-menu-close-button").hide(500);
-            display_menu = true;
-        } else {
-            $("#main_menu").animate({
-                left: "+=320",
-            }, 500);
-            $(".overlay").show(0);
-            $(".header-menu-close-button").show(500);
-            $(".header-menu-open-button").hide(500);
-            display_menu = false;
-        }
-    });
-
-    $('.overlay').click(function() {
-        console.log('click');
-        if (display_menu === false) {
-            $("#main_menu").animate({
-                left: "-=320",
-            }, 500);
-            $(".overlay").hide(0);
-            $(".header-menu-open-button").show(500);
-            $(".header-menu-close-button").hide(500);
-            display_menu = true;
-    }});
-}
-
-$(document).ready(function() {
-    menu_display();
-});
+require ('./modules/menu_display.js');
