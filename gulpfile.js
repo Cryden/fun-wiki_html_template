e6e3d1f4b853c3d2be0e4f16174b763eca860136
CRYDEsigN <Cryden_da@mail.ru>
2018-01-22 11:49:08 +0300

    add webpack stream

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,10 @@ const sass = require('gulp-sass');
 const twig = require('gulp-twig');
 const postcss = require('gulp-postcss');
 const groupmedia = require('gulp-group-css-media-queries');
+const webpack = require('webpack-stream');
 
 var dev = false;
+var dist_path = 'docs';
 
 // Main Tasks
 
@@ -24,7 +26,7 @@ gulp.task('sass', function () {
         postcss([
             require('cssnano')
         ])))
-    .pipe(gulp.dest('./docs/css'));
+    .pipe(gulp.dest('./' + dist_path + '/css'));
 });
 
 gulp.task('twig', function () {
@@ -34,7 +36,17 @@ gulp.task('twig', function () {
             title:   "Twig Error",
             message: "Error: <%= error.message %>"
         })))
-    .pipe(gulp.dest('./docs/'));
+    .pipe(gulp.dest('./' + dist_path + '/'));
+  });
+
+  gulp.task('js', function() {
+    return gulp.src('./source/js/app.js')
+    .pipe(webpack({
+          output: {
+            filename: '[name].js',
+          },
+        }))
+    .pipe(gulp.dest('docs/js'));
   });
 
 // Watch Tasks
